72806ebb3678e8ca25c5440c52257c0259c0bc89
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2026-01-13 04:55:57 +0100

    Humanizer and ContosoPizza

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Humanizer;
+// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-Console.WriteLine("Congratulations!");
-Console.WriteLine(" ");
-Console.Write("You have successfully created a .NET console application");
-Console.Write("!!!");
-Console.Write(" !!!");
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Times Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));    // zero cases
+    Console.WriteLine("case".ToQuantity(1));    // one case
+    Console.WriteLine("case".ToQuantity(5));    // five cases
+}
+
+static void HumanizeDates()
+{
+    DateTime pastDate = DateTime.UtcNow.AddHours(-24);
+    Console.WriteLine(pastDate.Humanize());  // 24 hours ago
+
+    DateTime futureDate = DateTime.UtcNow.AddDays(-2);
+    Console.WriteLine(futureDate.Humanize()); // in 30 minutes
+
+    TimeSpan timeSpan1 = TimeSpan.FromDays(1);
+    Console.WriteLine(timeSpan1.Humanize()); // in 30 minutes
+
+    TimeSpan timeSpan2 = TimeSpan.FromDays(16);
+    Console.WriteLine(timeSpan2.Humanize()); // in 30 minutes
+}
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33054626ab8cdd3123c94e9d3014be83abf88dde")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
