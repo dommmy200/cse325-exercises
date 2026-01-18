14ada6f77649b5b88a0deb1b877d5b6dea09d6c7
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2026-01-18 13:15:03 +0100

    RazorPage Assignment Submission

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -29,15 +30,57 @@ namespace RazorPagesMovie.Pages.Movies
 
         [BindProperty(SupportsGet = true)]
         public string? MovieGenre { get; set; }
+        [BindProperty(SupportsGet = true)]
+        [Range(1900, 2100, ErrorMessage = "Please enter a valid year")]
+        public int? SearchYear { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Range(1900, 2100, ErrorMessage = "Start year must be between 1900 and 2100")]
+        public int? YearFrom { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Range(1900, 2100, ErrorMessage = "End year must be between 1900 and 2100")]
+        public int? YearTo { get; set; }
+
+        public SelectList? Years { get; set; }
+
         public async Task OnGetAsync()
         {
+            // <snippet_search_linqQuery>
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            IQueryable<int> yearQuery = from m in _context.Movie
+                                        orderby m.ReleaseDate.Year
+                                        select m.ReleaseDate.Year;
             var movies = from m in _context.Movie
                          select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
-                movies = movies.Where(s => s.Title.Contains(SearchString));
+                movies = movies.Where(s => s.Title != null && s.Title.Contains(SearchString));
             }
-            Movie = await _context.Movie.ToListAsync();
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            if (SearchYear.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate.Year == SearchYear.Value);
+            }
+            // Year range filter
+            if (YearFrom.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate.Year >= YearFrom.Value);
+            }
+
+            if (YearTo.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate.Year <= YearTo.Value);
+            }
+            // <snippet_search_selectList>
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            // </snippet_search_selectList>
+            Movie = await movies.ToListAsync();
         }
     }
 }
