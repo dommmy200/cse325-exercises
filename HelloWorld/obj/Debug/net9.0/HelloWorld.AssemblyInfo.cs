83336c7600294cd9cc0ddc4720aef7988b31ad3a
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2026-01-22 10:22:39 +0100

    Started the BlazingPizza app

--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140c2245607ddfbeac85932d377bfbbd8a18790c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ada6f77649b5b88a0deb1b877d5b6dea09d6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
