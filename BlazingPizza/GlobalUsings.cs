4ecff9b7e659bd199f0b1fb8bf5f6d725c67cabd
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2026-01-24 12:16:16 +0100

    first commit

--- BlazingPizza/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using BlazingPizza.Model;
-global using BlazingPizza.Data;
-global using BlazingPizza.Services;
-global using Microsoft.EntityFrameworkCore;
-global using System.ComponentModel.DataAnnotations;
--- BlazingPizza/Model/UserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazingPizza.Model
-{
-    public class UserInfo
-    {
-        public bool IsAuthenticated { get; set; }
-
-        public string Name { get; set; }
-    }
-}
